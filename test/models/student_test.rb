c9c377e2f41af3642653ee1c05865f55506ce8ce
michelhuang6 <michelhuang6@gmail.com>
2016-09-25 16:11:14 -0400

    qkdqslkdql

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "minor student" do
+    titi= Student.find_by_name('titi')
+    assert_equal titi.major?, false
+  end
+  
+  test "major student" do
+    bibi= Student.find_by_name('bibi')
+    assert_equal bibi.major?, true
+  end
 end
