3c9520225d6579137d8db87937fb07412878063e
michelhuang6 <michelhuang6@gmail.com>
2016-10-18 04:03:45 -0400

    Modif de l'interface show

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,17 +6,15 @@ class City < ActiveRecord::Base
     def forecast_io
       forecast = ForecastIO.forecast(self.lat, self.lon, params: { units: 'si'})
 
-      result={}    
-
-      result[:current_wind]=forecast.currently.windSpeed
-
-      result[:icon]=forecast.currently.icon
-
-      result[:summary]=forecast.currently.summary
-
-      result[:current_temp]=forecast.currently.temperature
-
-result
+      result = {}    
+      result[:current_wind] = forecast.currently.windSpeed
+      result[:icon] = forecast.currently.icon
+      result[:summary] = forecast.currently.summary
+      result[:current_temp] = forecast.currently.temperature
+      result[:current_tempMin] = forecast.currently.temperatureMin
+      result[:current_tempMax] = forecast.currently.tempMax
+      result[:current_pressure] = forecast.currently.pressure
+      result
     end
   
   private
